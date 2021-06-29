69e7e400d200d4f571fc1527f3c6c65d3770336e
propheticwrit <job@vitalflohealth.com>
2021-06-28 21:25:43 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/clueo/mood_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.clueo.mood_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'app/home.dart';
+
+void main() {
+  runApp(Home());
+}
